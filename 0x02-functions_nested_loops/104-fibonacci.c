65c33ac34cdb138380ffdfd0cf68dc53713c4b39
cysof <cp4reallife@yahoo.com>
2023-07-18 20:54:02 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
+
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0.
  */
 
-/**
- * void- entry poin to function
- * description: and prints the Fibonacci sequence up to a given number n.
- *
- * @param n The number of Fibonacci numbers to generate and print.
- */
-
-void fibonacci_sequence(int n)
-{
-	int a = 1;
-	int b = 2;
-	int next_number;
-
-	printf("%d, %d", a, b);
-
-	for (int i = 3; i <= n; i++)
-	{
-		next_number = a + b;
-		printf(", %d", next_number);
-		a = b;
-		b = next_number;
-	}
-	printf("\n");
-}
-
 int main(void)
 {
-	fibonacci_sequence(98);
+	long int sum;
+	long int first, second;
+	int count;
+
+	sum = 0;
+	first = 0;
+	second = 1;
+	count = 0;
+
+	while (count < 49)
+	{
+		sum = first + second;
+		printf("%li, ", sum);
+		first = second;
+		second = sum;
+
+		count++;
+	}
+	sum = first + second;
+	printf("%li\n", sum);
+
 	return (0);
 }
-
