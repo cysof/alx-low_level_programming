6df3320d76ebb16a63bd6da41400796f3fe6c616
cysof <cp4reallife@yahoo.com>
2023-07-20 16:48:48 -0700

    fixing 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,68 +1,26 @@
-#include <math.h>
 #include "main.h"
 
 /**
- * power - exponents
- * @base: base
- * @exp: exponent
- * Return: result (int)
- */
-
-int  power(int base, int exp)
-{
-	int i, num;
-
-	num = 1;
-	for (i = 0; i < exp; ++i)
-		num *= base;
-
-	return (num);
-}
-
-/**
  * print_number - prints an integer
- * @n: number to print
- * Return void
+ * @n: integer to be printed
  */
-
 void print_number(int n)
 {
-	int negative = 0;
-	int digit;
-	int divisor;
-	int begin = 0;
-	int place = 10;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		negative = 1;
-		n = n * -1;
-	}
-	while (place >= 0)
+		n1 = -n;
+		_putchar('-');
+	} else
 	{
-		/*divisor = pow(10, place);*/
-		divisor = power(10, place);
-		digit = ((n / divisor) % 10);
-		if (digit == 0 && begin == 0)
-		{
-			place--;
-		}
-		else if (digit != 0 && begin == 0)
-		{
-			begin = 1;
-			if (negative == 1)
-				_putchar('-');
-			_putchar('0' + digit);
-			place--;
-		}
-		else
-		{
-			_putchar('0' + digit);
-			place--;
-		}
+		n1 = n;
 	}
-	if (digit == 0 && divisor == 1)
+
+	if (n1 / 10)
 	{
-		_putchar(48);
+		print_number(n1 / 10);
 	}
+
+	_putchar((n1 % 10) + '0');
 }
