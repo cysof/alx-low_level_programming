50b15943c8b1b07514bd7bf7595ea35a70a40cbd
cysof <cp4reallife@yahoo.com>
2023-07-25 02:09:58 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _puts(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
