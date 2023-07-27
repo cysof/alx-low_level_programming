db0e09c0aac9794d8c49512bb93d5fe7d5b34007
cysof <cp4reallife@yahoo.com>
2023-07-27 13:06:13 -0700

    103-infinite_add.c redo

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
  * infinite_add - Function that adds two numbers.
@@ -8,37 +8,33 @@
  * @r: The buffer to store the result.
  * @size_r: The size of the buffer.
  *
- * Return: A pointer to the result if the result can be stored
+ * Return: A pointer to the result if the result 0.
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0;
+	int len1 = 0, len2 = 0, sum, carry = 0;
 	int i, j;
 
+	while (n1[len1])
+		len1++;
+	while (n2[len2])
+		len2++;
 	if (len1 + 1 > size_r || len2 + 1 > size_r)
 		return (0);
-	i = len1 - 1;
-	j = len2 - 1;
-	r[size_r - 1] = '\0';
-	size_r--;
-	while (i >= 0 || j >= 0 || carry)
+	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
 	{
 		int digit1 = (i >= 0) ? n1[i] - '0' : 0;
 		int digit2 = (j >= 0) ? n2[j] - '0' : 0;
-		int sum = digit1 + digit2 + carry;
 
+		sum = digit1 + digit2 + carry;
 		carry = sum / 10;
-
-		if (size_r <= 0)
-			return (0);
 		r[size_r - 1] = (sum % 10) + '0';
 		size_r--;
-		i--;
-		j--;
 	}
-	return (r + size_r + 1);
+	while (size_r > 0)
+	{
+		r[size_r - 1] = r[size_r];
+		size_r--;
+	}
+	return (r);
 }
-
