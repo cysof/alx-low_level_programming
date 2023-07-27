cd3af0f5bac3aa007e123902cfe282e1fc0445af
cysof <cp4reallife@yahoo.com>
2023-07-27 12:17:39 -0700

    fixing up things 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,32 +4,15 @@
  * print_number - Function that prints an integer.
  * @n: The integer to be printed.
  */
-
 void print_number(int n)
 {
-	int reversed = 0;
-	int last_digit;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	while (n != 0)
-	{
-		last_digit = n % 10;
-		reversed = reversed * 10 + last_digit;
-		n /= 10;
-	}
-	while (reversed != 0)
-	{
-		_putchar(reversed % 10 + '0');
-		reversed /= 10;
-	}
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
 
