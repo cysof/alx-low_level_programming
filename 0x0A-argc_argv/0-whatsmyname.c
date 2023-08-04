b88cfd7a34194a39e149b516a4ca6121476bbd22
cysof <cp4reallife@yahoo.com>
2023-08-04 13:15:40 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: arguments vector
+ * Return: Always return 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
