7f611f373b2b329fff0a37c49e16302ba1cae2ae
cysof <cp4reallife@yahoo.com>
2023-08-10 08:43:12 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: Always return 1 on success.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
