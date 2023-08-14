9e313da2c7c756b7022c4e7b12f05dc54a980cd9
cysof <cp4reallife@yahoo.com>
2023-08-14 09:38:47 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - Entery point to the program
+ * @void: the paramiter to return
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf(" compiled from file: %s\n", __FILE__);
+	return (0);
+}
