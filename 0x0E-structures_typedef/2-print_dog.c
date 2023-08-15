c506cdf06029d152f454f7500ef3d911dd0e209f
cysof <cp4reallife@yahoo.com>
2023-08-14 20:45:46 -0700

    2-print_dog.c fixing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,29 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		printf("Age: %.1f\n", d->age);
-		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+		if (d->name == NULL)
+		{
+			printf("Name: %s\n", "(nil");
+		}
+		else
+		{
+			printf("Name : %s\n", d->name);
+		}
+		if (d == NULL)
+		{
+			printf("Age: %s\n", "(nil)");
+		}
+		else
+		{
+			printf("Age : %f\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner : %s\n", "(nil)");
+		}
+		else
+		{
+			printf("Owner : %s\n", d->owner);
+		}
 	}
 }
