d007242599280ee692625fa8cd968e2efd0332a6
cysof <cp4reallife@yahoo.com>
2023-08-17 07:20:21 -0700

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,11 +11,12 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, output;
+
 	if (array == NULL || cmp == NULL)
 	{
 		return (-1);
 	}
-	if(size <= 0)
+	if (size <= 0)
 	{
 		return (-1);
 	}
