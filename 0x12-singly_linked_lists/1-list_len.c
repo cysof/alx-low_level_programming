088285ff9274ebe8a4bc170527d1924b91ad1e17
cysof <cp4reallife@yahoo.com>
2023-08-27 15:49:27 -0700

    fixing

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
  * list_len - returns the number of elements in a linked list_t list
  * @h: head of linked list
  * Return: number of elements
  */
+
 size_t list_len(const list_t *h)
 {
 	size_t size;
@@ -14,5 +16,6 @@ size_t list_len(const list_t *h)
 		size++;
 		h = h->next;
 	}
+
 	return (size);
 }
