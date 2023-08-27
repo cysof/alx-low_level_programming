4fca4ffa4dff53f965018d5b291b294f95fcd56d
cysof <cp4reallife@yahoo.com>
2023-08-27 15:53:31 -0700

    fix up 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,9 @@ void __attribute__((constructor)) primary(void);
  * primary - print before the main function
  * Return: void
  */
+
 void primary(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon ");
 	printf("my back!\n");
 }
